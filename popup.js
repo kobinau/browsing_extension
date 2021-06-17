b7fcc63253017eed3a851f13f6c0fb276681aa27
Kobi Nauenberg <kobi.nauenberg@mail.utoronto.ca>
2021-06-17 13:12:28 -0400

    pulls Chrome history from buttons
    Signed-off-by: Kobi Nauenberg <kobi.nauenberg@mail.utoronto.ca>

--- popup.js
+++ popup.js
@@ -1,18 +1,20 @@
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
-
-chrome.storage.sync.get("color", ({ color }) => {
-changeColor.style.backgroundColor = color;
-});
+let get_history = document.getElementById("get_history");
+let refresh_history = document.getElementById("refresh_history");
 
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+get_history.addEventListener("click", async () => {
 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-	chrome.scripting.executeScript({
-	target: { tabId: tab.id },
-	function: setPageBackgroundColor,
-   });
+	
+chrome.history.search({text: '', maxResults: 100}, function(data) {
+    data.forEach(function(page) {
+        console.log(page);
+    });
+});
+//	chrome.scripting.executeScript({
+//	target: { tabId: tab.id },
+//	function: setPageBackgroundColor,
+ //  });
 });
 
 // The body of this function will be executed as a content script inside the
@@ -22,3 +24,12 @@ chrome.storage.sync.get("color", ({ color }) => {
 document.body.style.backgroundColor = color;
 });
 }
+
+refresh_history.addEventListener("click", async () => {
+//	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//	chrome.scripting.executeScript({
+//	target: { tabId: tab.id },
+	console.log("hello my name is kobi");
+//	function: getFullHistory,
+//   });
+});
